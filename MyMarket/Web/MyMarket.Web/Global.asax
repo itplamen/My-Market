e92fc63f62042aff558d919945e0706cbabaed37
itplamen@gmail.com <itplamen@gmail.com>
2017-04-03 09:10:30 +0300

    Created MyMarket.Web project (default ASP.NET MVC project with Web API)

---
+++ MyMarket/Web/MyMarket.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyMarket.Web.MvcApplication" Language="C#" %>
---
+++ MyMarket/Web/MyMarket.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMarket.Web.Startup))]
+namespace MyMarket.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
