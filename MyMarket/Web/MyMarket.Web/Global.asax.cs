a644938968478f33a01481973557296da90fae9d
itplamen@gmail.com <itplamen@gmail.com>
2017-04-09 19:16:59 +0300

    Installed Autofac dependency container

--- MyMarket/Web/MyMarket.Web/Global.asax.cs
+++ MyMarket/Web/MyMarket.Web/Global.asax.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-
-namespace MyMarket.Web
+﻿namespace MyMarket.Web
 {
+    using System.Web.Http;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+
+    using App_Start;
+    
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            AutofacConfig.RegisterAutofac();
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
