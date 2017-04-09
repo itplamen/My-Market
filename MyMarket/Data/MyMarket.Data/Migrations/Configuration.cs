ca63c0fd82501494776e1017ab029b740b2700c0
itplamen@gmail.com <itplamen@gmail.com>
2017-04-09 21:12:25 +0300

    Created InitialCreate migration

---
+++ MyMarket/Data/MyMarket.Data/Migrations/Configuration.cs
@@ -0,0 +1,22 @@
+namespace MyMarket.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    public sealed class Configuration : DbMigrationsConfiguration<MyMarket.Data.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            this.AutomaticMigrationsEnabled = false;
+            this.AutomaticMigrationDataLossAllowed = false;
+        }
+
+        protected override void Seed(MyMarket.Data.ApplicationDbContext context)
+        {
+             
+          
+        }
+    }
+}
--- MyMarket/Web/MyMarket.Web/Global.asax.cs
+++ MyMarket/Web/MyMarket.Web/Global.asax.cs
@@ -1,16 +1,21 @@
 ﻿namespace MyMarket.Web
 {
+    using System.Data.Entity;
     using System.Web.Http;
+    using System.Web;
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
 
     using App_Start;
-    
-    public class MvcApplication : System.Web.HttpApplication
+    using Data;
+    using Data.Migrations;
+
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
             AutofacConfig.RegisterAutofac();
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
