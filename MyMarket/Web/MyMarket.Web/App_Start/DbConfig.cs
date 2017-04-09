35044cfda5741fcb4b2406bc1f9fbbff1565bd95
itplamen@gmail.com <itplamen@gmail.com>
2017-04-10 02:19:00 +0300

    Added DbConfig

---
+++ MyMarket/Web/MyMarket.Web/App_Start/DbConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace MyMarket.Web.App_Start
+{
+    using System.Data.Entity;
+
+    using Data;
+    using Data.Migrations;
+
+    public class DbConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
+            ApplicationDbContext.Create().Database.Initialize(true);
+        }
+    }
+}
--- MyMarket/Web/MyMarket.Web/Global.asax.cs
+++ MyMarket/Web/MyMarket.Web/Global.asax.cs
@@ -1,31 +1,27 @@
 ﻿namespace MyMarket.Web
 {
-    using System.Data.Entity;
     using System.Reflection;
-    using System.Web.Http;
     using System.Web;
+    using System.Web.Http;
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
 
     using App_Start;
-    using Data;
-    using Data.Migrations;
     using Infrastructure.Mapping;
-
 
     public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
-            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
+            DbConfig.Initialize();
             AutofacConfig.RegisterAutofac();
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
+            
             var autoMapperConfig = new AutoMapperConfig();
             autoMapperConfig.Execute(Assembly.GetExecutingAssembly());
         }
