2f1a5f6d2814b2857a1ef6ef82e7403bc2309ae7
itplamen@gmail.com <itplamen@gmail.com>
2017-04-09 19:37:35 +0300

    Cleanup MyMarket solution by uninstalling NuGet packages

--- MyMarket/Web/MyMarket.Web/App_Start/GlimpseSecurityPolicy.cs
+++ MyMarket/Web/MyMarket.Web/App_Start/GlimpseSecurityPolicy.cs
@@ -4,7 +4,7 @@
 using Glimpse.AspNet.Extensions;
 using Glimpse.Core.Extensibility;
 
-namespace MyMarket.Web
+namespace MyMarket.Web.App_Start
 {
     public class GlimpseSecurityPolicy:IRuntimePolicy
     {
--- MyMarket/Web/MyMarket.Web/App_Start/Startup.Auth.cs
+++ MyMarket/Web/MyMarket.Web/App_Start/Startup.Auth.cs
@@ -49,23 +49,10 @@
             app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
 
             // Uncomment the following lines to enable logging in with third party login providers
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
-
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
 
             //app.UseFacebookAuthentication(
             //   appId: "",
             //   appSecret: "");
-
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
         }
     }
 }
