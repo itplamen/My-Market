6831c4b69dc25abb25aa9f3fa55824c1bf1c2c57
itplamen@gmail.com <itplamen@gmail.com>
2017-04-10 03:49:36 +0300

    Added MyMarket.Common project

---
+++ MyMarket/Common/MyMarket.Common/ValidationConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace MyMarket.Common
+{
+    public static class ValidationConstants
+    {
+        // User
+        public const int USER_NAME_MIN_LENGTH = 2;
+        public const int USER_NAME_MAX_LENGTH = 50;
+    }
+}
