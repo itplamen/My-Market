d8c965d6c68819eddb1448e41f75386751d81518
itplamen@gmail.com <itplamen@gmail.com>
2017-04-10 17:42:37 +0300

    Added ErrorMessages class to MyMarket.Common project

---
+++ MyMarket/Common/MyMarket.Common/ErrorMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace MyMarket.Common
+{
+    public static class ErrorMessages
+    {
+        // Common
+        public const string INVALID_ID = "ID is invalid!";
+        public const string ENTITY_CANNOT_BE_NULL = "Entity cannot be null!";
+    }
+}
