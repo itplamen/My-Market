742c9b485db2e30664182f9aedb57b3052c8e5d6
itplamen@gmail.com <itplamen@gmail.com>
2017-04-12 05:46:23 +0300

    Updated Home controller and Index view to show latest and most liked ads. They are cached for 15 minutes.

--- MyMarket/Web/MyMarket.Web/Controllers/HomeController.cs
+++ MyMarket/Web/MyMarket.Web/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿namespace MyMarket.Web.Controllers
 {
+    using System.Linq;
     using System.Web.Mvc;
 
     using Bytes2you.Validation;
 
+    using Models.Home;
+    using Infrastructure.Mapping;
     using Services.Data.Contracts;
 
     public class HomeController : BaseController
@@ -17,12 +20,27 @@
             this.adsService = adsService;
         }
 
+        [HttpGet]
         public ActionResult Index()
         {
+            var latestAds = this.Cache.Get(
+                "latestAds",
+                () => this.adsService.Latest().To<AdViewModel>().ToList(),
+                15 * 60);
 
-            return View();
+            var mostLikedAds = this.Cache.Get(
+                "mostLikedAds",
+                () => this.adsService.MostLiked().To<AdViewModel>().ToList(),
+                15 * 60);
+
+            var selectedAds = new SelectedAdsViewModel();
+            selectedAds.LatestAds = latestAds;
+            selectedAds.MostLiked = mostLikedAds;
+
+            return View(selectedAds);
         }
 
+        [HttpGet]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -30,6 +48,7 @@
             return View();
         }
 
+        [HttpGet]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
