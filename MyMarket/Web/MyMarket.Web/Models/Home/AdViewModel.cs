c27024a75dc027001a069d5aacbc1e061968b6f4
itplamen@gmail.com <itplamen@gmail.com>
2017-04-12 04:29:11 +0300

    Added AdViewModel and SelectedAdsViewModel

---
+++ MyMarket/Web/MyMarket.Web/Models/Home/AdViewModel.cs
@@ -0,0 +1,39 @@
+﻿namespace MyMarket.Web.Models.Home
+{
+    using System;
+
+    using AutoMapper;
+
+    using Data.Models;
+    using Infrastructure.Mapping;
+
+    public class AdViewModel : IMapFrom<Ad>, IHaveCustomMappings
+    {
+        public DateTime CreatedOn { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public byte[] Picture { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public int Visits { get; set; }
+
+        public int Likes { get; set; }
+
+        public int Comments { get; set; }
+
+        public void CreateMappings(IMapperConfiguration config)
+        {
+            config.CreateMap<Ad, AdViewModel>()
+                .ForMember(x => x.Category, opt => opt.MapFrom(x => x.Category.Name))
+                .ForMember(x => x.Visits, opt => opt.MapFrom(x => x.Visits.Count))
+                .ForMember(x => x.Likes, opt => opt.MapFrom(x => x.Likes.Count))
+                .ForMember(x => x.Comments, opt => opt.MapFrom(x => x.Comments.Count));
+        }
+    }
+}
---
+++ MyMarket/Web/MyMarket.Web/Models/Home/SelectedAdsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MyMarket.Web.Models.Home
+{
+    using System.Collections.Generic;
+
+    using Infrastructure.Mapping;
+
+    public class SelectedAdsViewModel : IMapFrom<AdViewModel>
+    {
+        public IEnumerable<AdViewModel> LatestAds { get; set; }
+
+        public IEnumerable<AdViewModel> MostLiked { get; set; }
+    }
+}
