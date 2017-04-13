a9fe74206488629b52a23e31dcf473cd60f0f08d
itplamen@gmail.com <itplamen@gmail.com>
2017-04-13 04:37:30 +0300

    Updated RouteConfig

--- MyMarket/Web/MyMarket.Web/App_Start/RouteConfig.cs
+++ MyMarket/Web/MyMarket.Web/App_Start/RouteConfig.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
-
-namespace MyMarket.Web
+﻿namespace MyMarket.Web
 {
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
@@ -14,10 +10,17 @@ namespace MyMarket.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "DefaultGetByid",
+                url: "{controller}/{id}",
+                defaults: new { action = "Get" },
+                constraints: new { id = @"\d+" },
+                namespaces: new[] { "MyMarket.Web.Controllers" });
+
+            routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                url: "{controller}/{action}/",
+                defaults: new { controller = "Home", action = "Index" },
+                namespaces: new[] { "MyMarket.Web.Controllers" });
         }
     }
 }
