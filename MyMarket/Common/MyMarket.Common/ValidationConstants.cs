25a0bb7b39ebdf5f75775bd48a3b058c3ea707da
itplamen@gmail.com <itplamen@gmail.com>
2017-04-14 23:14:04 +0300

    Updated constants ValidationConstants.cs naming convention

--- MyMarket/Common/MyMarket.Common/ValidationConstants.cs
+++ MyMarket/Common/MyMarket.Common/ValidationConstants.cs
@@ -3,14 +3,11 @@
     public static class ValidationConstants
     {
         // All entities
-        public const int INVALID_ID = 0;
-        public const int INVALID_COUNT = 0;
-        public const int NAME_MIN_LENGTH = 2;
-        public const int NAME_MAX_LENGTH = 50;
-        public const int CONTENT_MIN_LENGTH = 2;
-        public const int CONTENT_MAX_LENGHT = 250;
-
-        // Ads
-        public const int TOP_ADS_COUNT = 8;
+        public const int InvalidId = 0;
+        public const int InvalidCount = 0;
+        public const int NameMinLength = 2;
+        public const int NameMaxLenght = 50;
+        public const int ContentMinLength = 2;
+        public const int ContentMaxLength = 250;
     }
 }
