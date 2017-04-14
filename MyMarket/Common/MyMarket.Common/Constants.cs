2fbfe566d1eb714d796ade1fed55739baa108d60
itplamen@gmail.com <itplamen@gmail.com>
2017-04-15 02:41:05 +0300

    Added more Constants.cs to MyMarket.Common

---
+++ MyMarket/Common/MyMarket.Common/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace MyMarket.Common
+{
+    public static class Constants
+    {
+        // Ads
+        public const int TopAdsCount = 8;
+        public const int AdsPerPage = 15;
+        public const int AdsStartPage = 1;
+        public const string AdsInitialOrderBy = "CreatedOn";
+        public const string AscendingSortAds = "Ascending";
+        public const string DescendingSortAds = "Descending";
+    }
+}
