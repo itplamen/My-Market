7694ff13a070939114a3afcc29b890ceb96b56b8
itplamen@gmail.com <itplamen@gmail.com>
2017-04-15 10:01:08 +0300

    Created _AdsPartial with bootstrap pagination and AdError view models

---
+++ MyMarket/Web/MyMarket.Web/Models/Category/CategoryViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace MyMarket.Web.Models.Category
+{
+    using AutoMapper;
+
+    using Data.Models;
+    using Infrastructure.Mapping;
+
+    public class CategoryViewModel : IMapFrom<Category>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Ads { get; set; }
+
+        public void CreateMappings(IMapperConfiguration config)
+        {
+            config.CreateMap<Category, CategoryViewModel>()
+                .ForMember(x => x.Ads, opt => opt.MapFrom(x => x.Ads.Count));
+        }
+    }
+}
