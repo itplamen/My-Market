4a04b40c7a9af2143781aa22b609020a1070a0f5
itplamen@gmail.com <itplamen@gmail.com>
2017-04-15 04:22:25 +0300

    Created AdsSearchViewModel and AdsSearchResultViewModel

---
+++ MyMarket/Web/MyMarket.Web/Models/Ad/AdsSearchResultViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MyMarket.Web.Models.Ad
+{
+    using System.Collections.Generic;
+
+    using Infrastructure.Mapping;
+    
+    public class AdsSearchResultViewModel : IMapFrom<AdViewModel>
+    {
+        public IEnumerable<AdViewModel> Ads { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+    }
+}
---
+++ MyMarket/Web/MyMarket.Web/Models/Ad/AdsSearchViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace MyMarket.Web.Models.Ad
+{
+    using System.Collections.Generic;
+
+    using Common;
+
+    public class AdsSearchViewModel
+    {
+        public AdsSearchViewModel()
+        {
+            if (string.IsNullOrEmpty(this.SortBy))
+            {
+                this.SortBy = Constants.AdsInitialOrderBy;
+            }
+
+            if (string.IsNullOrEmpty(this.SortType))
+            {
+                this.SortType = Constants.AscendingSortAds;
+            }
+        }
+
+        public string SearchWord { get; set; }
+
+        public IEnumerable<int> ChosenCategoriesIds { get; set; }
+
+        public string SortBy { get; set; }
+
+        public string SortType { get; set; }
+
+        public bool IsSearchTermValid => !string.IsNullOrWhiteSpace(this.SearchWord) && this.SearchWord.Length >= Constants.MinimumTermLength;
+
+        public int MinimumSearchTermLength => Constants.MinimumTermLength;
+    }
+}
