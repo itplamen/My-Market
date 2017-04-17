fcece30a77ddccec570af1a7242a86cf9cf4af0e
itplamen@gmail.com <itplamen@gmail.com>
2017-04-17 09:45:28 +0300

    Created AdCreateViewModel view model

---
+++ MyMarket/Web/MyMarket.Web/Models/Ad/AdCreateViewModel.cs
@@ -0,0 +1,47 @@
+﻿namespace MyMarket.Web.Models.Ad
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using Category;
+    using Common;
+    using Data.Models;
+    using Infrastructure.Mapping;
+    using Validation;
+
+    public class AdCreateViewModel : IMapFrom<Ad>
+    {
+        [Required]
+        [AllowHtml]
+        [MinLength(ValidationConstants.NameMinLength)]
+        [MaxLength(ValidationConstants.NameMaxLenght)]
+        public string Title { get; set; }
+
+        [Required]
+        [AllowHtml]
+        [DataType(DataType.MultilineText)]
+        [MinLength(ValidationConstants.ContentMinLength)]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        [DataType(DataType.Currency, ErrorMessage = "Invalid price format. E.g. 20.99, 11.00, 9")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<CategoryViewModel> Categories { get; set; }
+
+        [Display(Name = "Main Picture")]
+        [CustomValidation(typeof(AdImageValidator), "ValidateImage")]
+        public HttpPostedFileBase MainPicture { get; set; }
+
+        [Display(Name = "Other Pictures")]
+        [CustomValidation(typeof(AdImageValidator), "ValidateImages")]
+        public IEnumerable<HttpPostedFileBase> OtherPictures { get; set; }
+    }
+}
