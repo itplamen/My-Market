f8a02f38acf0b26c6bcbaf1a2e5fdb25834787a0
itplamen@gmail.com <itplamen@gmail.com>
2017-04-17 07:49:29 +0300

    Created AdsController

--- MyMarket/Web/MyMarket.Web/Controllers/AdsController.cs
+++ MyMarket/Web/MyMarket.Web/Controllers/AdsController.cs
@@ -1,13 +1,21 @@
 ﻿namespace MyMarket.Web.Controllers
 {
     using System;
+    using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
+    using System.Web;
     using System.Web.Mvc;
+
+    using AutoMapper;
 
     using Bytes2you.Validation;
 
+    using Microsoft.AspNet.Identity;
+
     using Attributes;
     using Common;
+    using Data.Models;
     using Infrastructure.Mapping;
     using Models.Ad;
     using Models.Category;
@@ -17,24 +25,53 @@
     {
         private readonly IAdsService adsService;
         private readonly ICategoriesService categoriesService;
+        private readonly IImagesService imagesService;
 
-        public AdsController(IAdsService adsService, ICategoriesService categoriesService)
+        public AdsController(IAdsService adsService, ICategoriesService categoriesService, IImagesService imagesService)
         {
             Guard.WhenArgument(adsService, nameof(adsService)).IsNull().Throw();
             Guard.WhenArgument(categoriesService, nameof(categoriesService)).IsNull().Throw();
+            Guard.WhenArgument(imagesService, nameof(imagesService)).IsNull().Throw();
 
             this.adsService = adsService;
             this.categoriesService = categoriesService;
+            this.imagesService = imagesService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult Post()
+        {
+            return this.View(new AdCreateViewModel() { Categories = this.GetCategories() });
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Post(AdCreateViewModel createModel)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                createModel.Categories = this.GetCategories();
+
+                return this.View(createModel);
+            }
+
+            var adToCreate = Mapper.Map<Ad>(createModel);
+            adToCreate.UserId = this.User.Identity.GetUserId();
+
+            this.adsService.Add(adToCreate);
+
+            this.ProcessImages(createModel.MainPicture, createModel.OtherPictures, adToCreate.Id);
+
+            // TODO: Redirect to Ads/{id}
+            return RedirectToAction("Index");
         }
 
         [HttpGet]
         public ActionResult Index()
         {
-            var categories = this.categoriesService.All()
-                .To<CategoryViewModel>()
-                .ToList();
-
-            return this.View(categories);
+            return this.View(this.GetCategories());
         }
 
         [HttpGet]
@@ -61,32 +98,27 @@
         [ChildActionOnly]
         public ActionResult InitialAds()
         {
-            return this.Search(new AdsSearchViewModel(), Constants.AdsStartPage);
+            return this.Search(new AdsSearchViewModel(), Common.Constants.AdsStartPage);
         }
 
         [HttpPost]
         [AjaxOnly]
         public PartialViewResult SearchAds(AdsSearchViewModel searchModel, int? page)
         {
-            int actualPage = page ?? Constants.AdsStartPage;
-            Guard.WhenArgument(actualPage, nameof(actualPage)).IsLessThan(Constants.AdsStartPage).Throw();
+            int actualPage = page ?? Common.Constants.AdsStartPage;
+            Guard.WhenArgument(actualPage, nameof(actualPage)).IsLessThan(Common.Constants.AdsStartPage).Throw();
 
             return this.Search(searchModel, actualPage);
         }
 
-        private PartialViewResult Search(AdsSearchViewModel searchModel, int page)
+        private PartialViewResult Search(AdsSearchViewModel search, int page)
         {
-            Guard.WhenArgument(page, nameof(page)).IsLessThan(Constants.AdsStartPage).Throw();
+            Guard.WhenArgument(page, nameof(page)).IsLessThan(Common.Constants.AdsStartPage).Throw();
 
             var allAdsCount = this.adsService.All().Count();
-            var totalPages = (int)Math.Ceiling(allAdsCount / (decimal)Constants.AdsPerPage);
+            var totalPages = (int)Math.Ceiling(allAdsCount / (decimal)Common.Constants.AdsPerPage);
 
-            var ads = this.adsService.Search(
-                searchModel.SearchWord, 
-                searchModel.ChosenCategoriesIds, 
-                searchModel.SortBy, 
-                searchModel.SortType, 
-                page)
+            var ads = this.adsService.Search(search.SearchWord, search.ChosenCategoriesIds, search.SortBy, search.SortType, page)
                 .To<AdViewModel>()
                 .ToList();
 
@@ -98,6 +130,59 @@
             };
 
             return this.PartialView("_AdsPartial", adsListViewModel);
+        }
+
+        private IEnumerable<CategoryViewModel> GetCategories()
+        {
+            return this.Cache.Get(
+                "categories",
+                () => this.categoriesService.All()
+                    .OrderBy(c => c.Name)
+                    .To<CategoryViewModel>()
+                    .ToList(),
+                15 * 60);
+        }
+
+        private void ProcessImages(HttpPostedFileBase mainImage, IEnumerable<HttpPostedFileBase> otherImages, int adId)
+        {
+            if (mainImage != null)
+            {
+                this.UploadImage(mainImage, adId);
+            }
+
+            if (otherImages != null && otherImages.Any())
+            {
+                foreach (var img in otherImages)
+                {
+                    if (img != null)
+                    {
+                        this.UploadImage(img, adId);
+                    }
+                }
+            }
+        }
+
+        private void UploadImage(HttpPostedFileBase image, int adId)
+        {
+            string fileExtension = Path.GetExtension(image.FileName);
+            string fileName = string.Format("{0}.{1}", Guid.NewGuid(), fileExtension);
+            string path = Path.Combine(this.Server.MapPath(@"~/Content/Images/Ads/"), fileName);
+            image.SaveAs(path);
+
+            this.SaveImage(fileExtension, image.FileName, path, adId);
+        }
+
+        private void SaveImage(string fileExtension, string originalFileName, string urlPath, int adId)
+        {
+            var image = new Image()
+            {
+                FileExtension = fileExtension,
+                OriginalFileName = originalFileName,
+                UrlPath = urlPath,
+                AdId = adId
+            };
+
+            this.imagesService.Add(image);
         }
     }
 }
