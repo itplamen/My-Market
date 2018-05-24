55a7808d4d5420ce392a15988cf2bc4b10106d78
itplamen <itplamen@gmail.com>
2018-05-24 13:53:30 +0300

    Added users area

---
+++ MyMarket/Web/MyMarket.Web/Areas/Users/Controllers/ProfileController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace MyMarket.Web.Areas.Users.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [HttpGet]
+        [Authorize]
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ MyMarket/Web/MyMarket.Web/Areas/Users/UsersAreaRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace MyMarket.Web.Areas.Users
+{
+    public class UsersAreaRegistration : AreaRegistration
+    {
+        public override string AreaName => "Users";
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "UsersProfile",
+                "Users/Profile/{action}",
+                new { controller = "Profile", action = "Index", id = UrlParameter.Optional });
+        }
+    }
+}
